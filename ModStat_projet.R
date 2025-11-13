1dc85521eb9015d670112fa8e369ec6db4f79a02
jzitounifla <jzitounifla@ensc.fr>
2025-11-13 11:02:47 +0100

    Julia -> travail sur ACP

--- ModStat_projet.R
+++ ModStat_projet.R
@@ -42,15 +42,9 @@ activation <- readRDS('activation.Rdata')
 
 # PARTIE 1 : VISUALISATION DES DONNÉES -------------------------------
 
-resACP <- PCAmix(activation[,-(1:2)], graph=FALSE)
 
-cat('\n\n--------- ACP -----------\n\n\tCalcul des Eingen values :\n\n')
-# Pour préparer l'ACP, on regarde les Eingen values
-print(round(resACP$eig, digits=2))
-
-# on plot jusqu'à la dimension 5. 
-plot(resACP,choice="cor", axes=c(1:5)) 
-
+activation_H <- activation[activation$Sexe=="H",]
+activation_F <- activation[activation$Sexe=="F",]
 
 
 
@@ -59,6 +53,27 @@ plot(resACP,choice="cor", axes=c(1:5))
 
 
 
+resACP <- PCAmix(activation[,-(1:2)], graph=FALSE)
+
+
+cat('\n\n--------- ACP -----------\n\n\tCalcul des Eingen values :\n\n')
+# Pour préparer l'ACP, on regarde les Eingen values
+print(round(resACP$eig, digits=2))
+
+# et mtnt on plot
+plot(resACP,axes=c(1,2), coloring.ind=activation$Sexe, main="ACP, répartition H-F pour dim 1-2")
+plot(resACP,axes=c(1,2),choice="cor", lim.cos2.plot = 0.2, main="ACP dim 1-2")
+plot(resACP,axes=c(3,4),choice="cor", lim.cos2.plot = 0.2, main="ACP dim 3-4")
+plot(resACP,axes=c(1,3),choice="cor", lim.cos2.plot = 0.2,main="ACP dim 1-3")
+
+
+plot(activation$PROD_G_Frontal_Inf_Tri_1_L, activation$PROD_G_Rolandic_Oper_1_L)
+plot(activation$PROD_G_Frontal_Inf_Tri_1_L, activation$PROD_S_Sup_Temporal_4_L)
+plot(activation$PROD_G_Frontal_Inf_Tri_1_L, activation$PROD_G_Frontal_Inf_Tri_1_R)
+
+
+# et on regarde le détail de quelle variable apparait sur quoi 
+print(round(resACP$quanti$cos2, digits=2))
 
 # PARTIE 3 :  -------------------------------
 
